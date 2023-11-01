7efd1a45596b5fdd3d7acd72cc2705a5805fbc9b
AswinVJ666 <aswinvjanakan@gmail.com>
2023-11-01 23:02:21 +0530

    1/11/23

---
+++ script/js1.js
@@ -0,0 +1,41 @@
+// console.log('hello world');
+// // let name = 'java';
+// // console.log(`name is $(name)`);
+// let person = {
+//     name : 'jhon',
+//     age : '40'
+// };
+// console.log(person);
+// person.name = 'wick';
+// console.log(person);
+// // arrays
+// let fruitsName = ['apple','orange','mango'];
+// console.log(fruitsName);
+// // console.log(fruitsName[0]);
+// let num = 20;
+// console.log(num/=2);
+
+
+
+       // user input***************
+
+// let firstName = window.prompt("enter your name:");
+// let age = window.prompt('what is your age:');
+
+// document.getElementById('first').innerHTML = 'Hello ' + firstName;
+// document.getElementById('second').innerHTML = 'your Age is ' + age;
+
+
+
+
+
+
+      //html input***************
+
+// document.getElementById('submitButton').onclick =  function(){
+//     username = document.getElementById('textInput').value;
+//     console.log(username);
+//     document.getElementById('first').innerHTML = 'Welcome '+ username ;
+// };
+
+
