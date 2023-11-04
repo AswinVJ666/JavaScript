5d6b274ac09c145f249c69cae695a0c0ea3fb0a3
AswinVJ666 <aswinvjanakan@gmail.com>
2023-11-04 16:25:32 +0530

    4/11/23

--- script/js1.js
+++ script/js1.js
@@ -36,6 +36,42 @@
 //     username = document.getElementById('textInput').value;
 //     console.log(username);
 //     document.getElementById('first').innerHTML = 'Welcome '+ username ;
+//     document.getElementById('textInput').value ='';
 // };
+
+
+
+
+                  // math*********
+
+
+// let x = 51.67 ;
+
+// let y = 30.32 ; 
+
+// let z = -40 ; 
+
+// x = Math.round(x); rounds off  
+
+// x = Math.floor(x) ; rounds to lower 
+
+// x = Math.ceil(x) ; rounds to upper
+
+// x = Math.pow(x,2); calc the power
+
+// x = Math.sqrt(x); square root 
+
+// x = Math.abs(z);  magnitude or positive value 
+
+// console.log(Math.max(x,y,z));  prints the max value 
+
+
+// console.log(Math.min(x,y,z)); prints the min value
+
+// console.log(x);
+
+
+
+
 
 
